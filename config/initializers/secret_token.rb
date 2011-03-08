01307b8edc9de6ff8dc39be35270de7aba8f2cf5
Ryan Ferretti <ryanferretti@gmail.com>
2011-03-07 19:02:27 -0600

    first commit, simple base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EVenti::Application.config.secret_token = 'e2295210f2454539e4f712ccaeaf614c690ebf7cde15c0de59c16bef1756607261423e4a751c47b8438040c9cb9222c46097ac3c118aa6fc788de3b1eb6c0ba7'
