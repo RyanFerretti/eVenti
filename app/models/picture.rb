b7dd0d8bc88edbada38fed4b3daa17781397ebe5
Ryan Ferretti <ryanferretti@gmail.com>
2011-03-30 16:40:18 -0500

    added watermark

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,11 @@
+require 'paperclip_processors/watermark'
+
 class Picture < ActiveRecord::Base
   belongs_to :member
-  has_attached_file :image, :styles => { :thumb => "50x50>", :small => "100x100>", :original => "800x600>" }
+  has_attached_file :image, :processors => [:watermark],
+                    :styles => { :thumb => "50x50>", :small => "100x100>", :original => "800x600>",
+                                 :watermarked => { :geometry => '640x480>', :watermark_path => "#{Rails.root}/public/images/watermark.png"}
+                    }
   attr_accessible :image, :approved, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
 
 end
