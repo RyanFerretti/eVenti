36ca52cedc7d466f07e355588983b5623640ba91
Ryan Ferretti <ryanferretti@gmail.com>
2011-05-24 22:27:26 -0500

    heroku seems out of sync

--- app/models/member_summary.rb
+++ app/models/member_summary.rb
@@ -5,7 +5,7 @@ class MemberSummary < ActiveRecord::Base
   attr_accessible :profile_name, :date_of_birth, :address, :city, :state, :zip_code, :tour_market, :facebook_url,
                   :telephone, :bust, :waist, :hips, :piercings, :piercing_description, :tattoos, :tattoo_description,
                   :first_concert, :last_concert, :top_bands, :favorite_song, :relationship_status, :favorite_sport,
-                  :talent, :height, :weight, :hair_color
+                  :talent, :height, :weight, :hair_color 
 
 
   
