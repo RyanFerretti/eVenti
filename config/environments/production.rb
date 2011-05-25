87ab9075d81ae683fd04dda23d741ed97974fa9d
Ryan Ferretti <ryanferretti@gmail.com>
2011-05-25 09:16:15 -0500

    adding host to config to test email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ EVenti::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'dev-msuproar.heroku.com' }
 
   # Enable threaded mode
   # config.threadsafe!
