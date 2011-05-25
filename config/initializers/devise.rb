3023daafeff75319e6de671112c4bd9fd295df7e
Ryan Ferretti <ryanferretti@gmail.com>
2011-05-25 11:10:44 -0500

    changed sender to see if it will work on gmail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "webmaster@rockstaruproar.com"
+  config.mailer_sender = "webmaster@dev-msuproar.heroku.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
