90b3106b461c67dc8f19373e903b5123ba729af3
RyanFerretti <ryanferretti@gmail.com>
2011-06-03 09:14:32 -0700

    fixing validation for the form... need to test

--- public/javascripts/sliding.form.js
+++ public/javascripts/sliding.form.js
@@ -1,80 +1,53 @@
 $(function() {
-	/*
-	number of fieldsets
-	*/
-	var fieldsetCount = $('form').children().length;
-	
-	/*
-	current position of fieldset / navigation link
-	*/
-	var current 	= 1;
-    
-	/*
-	sum and save the widths of each one of the fieldsets
-	set the final sum as the total width of the steps element
-	*/
-	var stepsWidth	= 0;
-    var widths 		= new Array();
+
+	var fieldsetCount = $('form').children().length,
+		current	= 1,
+		stepsWidth = 0,
+		widths = new Array();
+		
 	$('#steps .step').each(function(i){
-        var $step 		= $(this);
-		widths[i]  		= stepsWidth;
-        stepsWidth	 	+= $step.width();
+        var $step = $(this);
+		widths[i] = stepsWidth;
+        stepsWidth += $step.width();
     });
 	$('#steps').width(stepsWidth);
-	
-	/*
-	to avoid problems in IE, focus the first input of the form
-	*/
+		
+	// to avoid problems in IE, focus the first input of the form
 	$('form').children(':first').find(':input:first').focus();
-	
-	/*
-	show the navigation bar
-	*/
 	$('#navigation').show();
 	
-	/*
-	when clicking on a navigation link 
-	the form slides to the corresponding fieldset
-	*/
+	// slide to the corresponding fieldset
     $('#navigation a').bind('click',function(e){
-		var $this	= $(this);
-		var prev	= current;
-        var isValid = true;
+		var $this = $(this),
+			prev = current,
+			isValid = true;
 		$this.closest('ul').find('li').removeClass('selected');
         $this.parent().addClass('selected');
-		/*
-		we store the position of the link
-		in the current variable	
-		*/
 		current = $this.parent().index() + 1;
-		/*
-		animate / slide to the next or to the corresponding
-		fieldset. The order of the links in the navigation
-		is the order of the fieldsets.
-		Also, after sliding, we trigger the focus on the first 
-		input element of the new fieldset
-		If we clicked on the last link (confirmation), then we validate
-		all the fieldsets, otherwise we validate the previous one
-		before the form slided
-		*/
-
+		
+		// validate all if at the end
 	    if(current == fieldsetCount) {
 		    isValid = validateSteps();
         }
 		else {
 			isValid = validateStep(prev);
         }
-        if(!isValid){
+        if(isValid){
+			// set selected tab
+			$this.closest('ul').find('li').removeClass('selected');
+			$this.parent().addClass('selected');
+			// focus on first input
 		    $('form').children(':nth-child('+ parseInt(current) +')').find(':input:first').focus();
+			// animate
             $('#steps').stop().animate({ marginLeft: '-' + widths[current-1] + 'px' },500);
         }
+		else {
+			current = prev
+		}
         e.preventDefault();
     });
 	
-	/*
-	clicking on the tab (on the last input of each fieldset), makes the form
-	slide to the next step
-	*/
+	// pressing tab on the last input will slide to the next fieldset
 	$('form > fieldset').each(function(){
 		var $fieldset = $(this);
 		$fieldset.children(':last').find(':input').keydown(function(e){
@@ -87,95 +60,55 @@ $(function() {
 		});
 	});
 	
-	/*
-	validates errors on all the fieldsets
-	records if the Form has errors in $('#formElem').data()
-	*/
+	// validates errors on all the fieldsets
 	function validateSteps(){
-		var FormErrors = false;
+		var isValid = true;
 		for(var i = 1; i < fieldsetCount; ++i){
-			var error = validateStep(i);
-			if(error == -1)
-				FormErrors = true;
+			if(!validateStep(i)){
+				isValid = false;
+			}
 		}
-		$('form').data('errors',FormErrors);
-        return FormErrors;
+		$('form').data('errors',!isValid);
+        return isValid;
 	}
 	
-	/*
-	validates one fieldset
-	and returns -1 if errors found, or 1 if not
-	*/
+	// validates one fieldset
 	function validateStep(step){
-        step++;
-		if(step == fieldsetCount) return;
-		var error = 1;
-		var hasError = false;
-		$('form').children(':nth-child('+ parseInt(step) +')').find(':input:not(button)').each(function(){
-			var $this 		= $(this);
-			var valueLength = jQuery.trim($this.val()).length;
-            var isValid = valueLength != '';
-            if($this.hasClass("confirmable")){
-                isValid = isValid && confirmConfirmable($this);
-            }
-			validateObject($this,isValid);
-            if(!isValid){
-               hasError = true;
-            }
-		});
-		var $link = $('#navigation li:nth-child(' + parseInt(step-1) + ') a');
-		$link.parent().find('.error,.checked').remove();
-		
-		var valclass = 'checked';
-		if(hasError){
-			error = -1; 
-			valclass = 'error';
-		}
-		$('<span class="'+valclass+'"></span>').insertAfter($link);
-		
-		return error;
-	}
-	
-	/*
-	if there are errors don't allow the user to submit
-	*/
-	$('form').live('submit',function(e){
-		if($('#navigation .errors').length){
-			alert('Please correct the errors in the Form');
-            //e.preventDefault();
+        var isValid = true,
+			link = $('#navigation li:nth-child(' + parseInt(step-1) + ') a'),
+			valclass = 'checked';
+		step++;
+		if(step == fieldsetCount){
 			return true;
 		}
-        if(!checkRequiredCheckBoxes()){
-			alert('Please accept the Rules and Terms of Conditions');
-            e.preventDefault();
-			return false;
+		$('form').children(':nth-child('+ parseInt(step) +')').find(':input:not(button)').each(function(){
+			if(!validateInput($(this))){
+               isValid = false;
+            }
+		});
+		link.parent().find('.error,.checked').remove();
+		if(!isValid){
+			valclass = 'error';
 		}
-	});
-	$("fieldset:not(:last-child)").append('<a href="#" class="next-button">Next</a>');
-
-    $("fieldset a").live("click",function(e){
-        var current = $(this).parent().index(),
-            atag = $('#navigation li a')[current];
-        $(atag).click();
-        e.preventDefault();
-        return false;
-    });
-
-    $(".radios input[type=radio]").live("click",function(){
-        if($(this).val()=="yes"){
-            $(this).parent().siblings().last().attr("disabled","");
-        }
-        else {
-            $(this).parent().siblings().last().attr("disabled","disabled");
-        }
-    });
-
-    $(".required-for-submit").live("click",function(){
-        checkRequiredCheckBoxes();
-    });
-
-    appendMessages();
-    mirrorTyping();
+		$('<span class="'+valclass+'"></span>').insertAfter(link);
+		return isValid;
+	}
+	
+	// validate object
+	function validateInput(obj){
+		var isValid = true,
+			val = jQuery.trim($this.val());
+		// required
+		if($this.hasClass("required")){
+			isValid = isValid && val != "";
+		}
+		// confirmed
+		if($this.hasClass("confirmable")){
+			isValid = isValid && confirmConfirmable($this);
+		}
+		showValidationErrorsIfNeeded($this,isValid);
+		return isValid;
+	}
 
     function checkRequiredCheckBoxes(){
         var total = $(".required-for-submit").length;
@@ -211,33 +144,69 @@ $(function() {
             var obj = $(this);
             appendMessageToParent(obj)
         });
-
     }
 
     function appendMessageToParent(obj){
         obj.parent().append("<span class=\"message\">" + obj.attr("data-message") + "</span>");
     }
 
-    $(".confirmable").live("blur",function(e){
-        confirmConfirmable($(this));
-    });
-
     function confirmConfirmable(obj){
         var other = obj.parent().prev().children("input").first();
-        return validateObject(obj,obj.val() == other.val());
+        return obj.val() == other.val();
     }
 
-    function validateObject(obj,isValid){
-        if(!isValid){
-            obj.css('background-color','#FFEDEF');
-            obj.css('border-color','#cc0000');
-        }
-        else {
+    function showValidationErrorsIfNeeded(obj,isValid){
+        if(isValid){
             obj.css('background-color','#FFFFFF');
             obj.css('border-color','');
         }
-        return isValid;
+        else {
+            obj.css('background-color','#FFEDEF');
+            obj.css('border-color','#cc0000');
+        }
     }
+	
+    appendMessages();
+    mirrorTyping();
+
+	$('form').live('submit',function(e){
+		if($('#navigation .errors').length){
+			alert('Please correct the errors in the Form');
+            return true;
+		}
+        if(!checkRequiredCheckBoxes()){
+			alert('Please accept the Rules and Terms of Conditions');
+            e.preventDefault();
+			return false;
+		}
+	});
+	$("fieldset:not(:last-child)").append('<a href="#" class="next-button">Next</a>');
+
+    $("fieldset a").live("click",function(e){
+        var current = $(this).parent().index(),
+            atag = $('#navigation li a')[current];
+        $(atag).click();
+        e.preventDefault();
+        return false;
+    });
+
+    $(".radios input[type=radio]").live("click",function(){
+        if($(this).val()=="yes"){
+            $(this).parent().siblings().last().attr("disabled","");
+        }
+        else {
+            $(this).parent().siblings().last().attr("disabled","disabled");
+        }
+    });
+
+    $(".required-for-submit").live("click",function(){
+        checkRequiredCheckBoxes();
+    });
+
+    $(".confirmable").live("blur",function(e){
+		var obj = $(this);
+		showValidationErrorsIfNeeded(obj,confirmConfirmable(obj));
+    });
 
     $(".required").each(function(){
         $(this).after("<span class=\"red\">*</span>");
