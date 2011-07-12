6a96d6d3d323560e368fd5eae766a9d15edfe258
Ryan Ferretti <ryanferretti@gmail.com>
2011-07-11 22:52:29 -0500

    more changes for caching

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,7 +9,7 @@ class Location < ActiveRecord::Base
   scope :ref_data, select("id, city, state")
 
   scope :active, where(:active => true)
-  scope :c_active, Rails.cache.fetch(:location){self.active.to_a}
+  scope :c_active, Rails.cache.fetch(:location){self.active.all}
 
   def name
     "#{city}, #{state}"
