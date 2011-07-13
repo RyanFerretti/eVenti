051b0789379797243fb03b33fada4d1d6b711277
Ryan Ferretti <ryanferretti@gmail.com>
2011-07-13 18:28:42 -0500

    removed recaptcha stuff

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -1,13 +1,6 @@
 class Members::RegistrationsController < Devise::RegistrationsController
   def create
-    if verify_recaptcha
-      super
-    else
-      build_resource
-      flash[:alert] = "There was an error with the captcha code below. Don't worry, all of your info is still here. We just need you to re-select your pictures for upload and enter the code again, then click Register."
-      @recaptcha_error = true
-      respond_with_navigational(resource){ render_with_scope :new }
-    end
+    super
   end
 
   protected
