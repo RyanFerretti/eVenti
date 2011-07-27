601ef70445cf8937ab9215b7e4471b6af1d93d8c
Ryan Ferretti <ryanferretti@gmail.com>
2011-07-26 20:43:16 -0500

    added caching to the main admin show page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
 
   def show_members
+    cache_5
     @members = Member.find :all, :include => [:pictures, :member_summary]
   end
 end
