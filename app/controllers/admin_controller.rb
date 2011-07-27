3f6abe172f9f915e3fa178a4ae489a88ef4bcdd7
Ryan Ferretti <ryanferretti@gmail.com>
2011-07-26 20:58:57 -0500

    testing caching

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
 
   def show_members
+    logger.error "caching!"
     cache_5
     @members = Member.find :all, :include => [:pictures, :member_summary]
   end
