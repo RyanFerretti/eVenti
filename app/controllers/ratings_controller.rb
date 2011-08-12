f4ca28ca0b3d8f07ea3525227a137663001a3b43
Ryan Ferretti <ryanferretti@gmail.com>
2011-08-12 10:27:29 -0500

    cleaned up ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,15 +1,18 @@
 class RatingsController < ApplicationController
+  before_filter :setup_rating, :only => [:create]
 
-  # POST /ratings
-  # POST /ratings.xml
   def create
-    rating = params[:vote_value].to_i || 5
-    rating = 5 if rating < 1 || rating > 10
-    
     @current = Member.find(params[:member_id])
-    @current.ratings.create(:value => rating)
+    @current.ratings.create(:value => @rating)
     @next = @current.next(state_to_display)
-    
     redirect_to show_user_profile_path(:profile_name => @next.profile_name, :prev => @current.id)
+  end
+
+  private
+
+  def setup_rating
+    default_rating = 5
+    @rating = params[:vote_value].to_i || default_rating
+    @rating = default_rating if @rating < 1 || @rating > 10
   end
 end
