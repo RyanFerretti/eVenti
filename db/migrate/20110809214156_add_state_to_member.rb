5294b915bb2d6860e46f5b3b1e9e1988fca5f96f
Ryan Ferretti <ryanferretti@gmail.com>
2011-08-11 19:06:36 -0500

    added default to pending

--- db/migrate/20110809214156_add_state_to_member.rb
+++ db/migrate/20110809214156_add_state_to_member.rb
@@ -1,6 +1,6 @@
 class AddStateToMember < ActiveRecord::Migration
   def self.up
-    add_column :users, :state, :string
+    add_column :users, :state, :string, :default => "pending"
   end
 
   def self.down
--- db/migrate/20110810061948_update_states_to_pending.rb
+++
@@ -1,8 +0,0 @@
-class UpdateStatesToPending < ActiveRecord::Migration
-  def self.up
-    Member.update_all "state = 'pending'", "state is null"
-  end
-
-  def self.down
-  end
-end
