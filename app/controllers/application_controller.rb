c5eae7579d562be6728e5fdb328e9fea63c684c0
Ryan Ferretti <ryanferretti@gmail.com>
2011-08-11 19:54:39 -0500

    changed admin root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || (resource.is_a?(Admin)? admins_locations_report_path : edit_member_registration_path)
+    stored_location_for(resource) || (resource.is_a?(Admin)? admins_root_path : edit_member_registration_path)
   end
 end
